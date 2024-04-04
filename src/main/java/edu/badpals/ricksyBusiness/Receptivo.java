1de0246e39fd5e59cba2bc8435654eb67e5d4e2e
AmarinoLama <amarinolama@gmail.com>
2024-04-04 12:51:06 +0200

    feat (Receptivo) : añadir el método dispatch

--- src/main/java/edu/badpals/ricksyBusiness/Receptivo.java
+++ src/main/java/edu/badpals/ricksyBusiness/Receptivo.java
@@ -1,4 +1,21 @@
 package edu.badpals.ricksyBusiness;
 
+import java.util.ArrayList;
+
 public class Receptivo {
+
+    private ArrayList<GuestDispatcher> observers = new ArrayList<>();
+
+    public Receptivo() {
+    }
+
+    public void registra(GuestDispatcher observer){
+        observers.add(observer);
+    }
+
+    public void dispatch(CreditCard tarjeta) {
+        for (GuestDispatcher paquete : observers) {
+            paquete.dispatch(tarjeta);
+        }
+    }
 }
